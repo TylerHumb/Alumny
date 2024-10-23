385d2de337063748a70b157f40ca3543f18fcccd
TylerHumb <101156725+TylerHumb@users.noreply.github.com>
2024-10-23 15:11:31 +1100

    renamed testlogic

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import testlogic
+import Repository
 import claude
 from flask import jsonify,abort
 app = Flask(__name__)
@@ -10,19 +10,19 @@ def index():
 
 @app.route("/loginemp/<int:id>")
 def loginemp(id):
-    return testlogic.loginEmployee(id)
+    return Repository.loginEmployee(id)
 
 @app.route('/skillsemp/<int:id>')
 def skillsemp(id):
-    return testlogic.getSkillsEmployee(id)
+    return Repository.getSkillsEmployee(id)
 
 @app.route('/addskillemp/<int:id>/<string:skill>')
 def addSkillemp(id,skill):
-    return testlogic.addSkillToEmployee(id,skill)
+    return Repository.addSkillToEmployee(id,skill)
 
 @app.route('/deleteskillemp/<int:id>/<string:skill>')
 def deleteSkillemp(id,skill):
-    return testlogic.deleteSkillEmployee(id,skill)
+    return Repository.deleteSkillEmployee(id,skill)
 
 @app.route('/extractskillsemp/<int:id>')
 def extractSkillemp(id):
@@ -35,52 +35,52 @@ def extractSkillemp(id):
 
 @app.route('/deleteallemp/<int:id>')
 def deleteallemp(id):
-    return testlogic.deleteAllEmployee(id)
+    return Repository.deleteAllEmployee(id)
 
 @app.route('/createemp/<string:name>', methods=['POST'])
 def createemployee(name):
-    return testlogic.createEmployee(name)
+    return Repository.createEmployee(name)
 
 
 @app.route('/resume/<int:id>/<string:resume>', methods=['POST'])
 def updateresume(id,resume):
-    return testlogic.setresume(id,resume)
+    return Repository.setresume(id,resume)
 
 @app.route('/removeresume/<int:id>')
 def removeresume(id):
-    return testlogic.clearresume(id)
+    return Repository.clearresume(id)
 
 @app.route('/deleteemp/<int:id>')
 def deleteemployee(id):
-    return testlogic.deleteEmployee(id)
+    return Repository.deleteEmployee(id)
 
 @app.route('/loginbus/<int:id>')
 def loginBusiness(id):
-    return testlogic.loginBusiness(id)
+    return Repository.loginBusiness(id)
 
 @app.route('/listings/<int:id>')
 def getListings(id):
-    return testlogic.getListings(id)
+    return Repository.getListings(id)
 
 @app.route('/skillsbus/<int:id>')
 def skillsbus(id):
-    return testlogic.getSkillsBusiness(id)
+    return Repository.getSkillsBusiness(id)
 
 @app.route('/createbus/<string:name>')
 def createbus(name):
-    return testlogic.createBusiness(name)
+    return Repository.createBusiness(name)
 
 @app.route('/addskillbus/<int:id>/<string:skill>')
 def addskillbus(id,skill):
-    return testlogic.addSkillBusiness(id,skill)
+    return Repository.addSkillBusiness(id,skill)
 
 @app.route('/deleteskillbus/<int:id>/<string:skill>')
 def deleteskillbus(id,skill):
-    return testlogic.deleteSkillBusiness(id,skill)
+    return Repository.deleteSkillBusiness(id,skill)
 
 @app.route('/deleteallbus/<int:id>')
 def deleteallbus(id):
-    return testlogic.deleteAllBusiness(id)
+    return Repository.deleteAllBusiness(id)
 
 @app.route('/extractskillsbus/<int:id>')
 def extractskillsbus(id):
@@ -95,21 +95,21 @@ def extractskillsbus(id):
 def deletelisting(id):
     try:
         deleteallbus(id) #delete all the skills related to the listing first
-        return testlogic.deleteListing(id)
+        return Repository.deleteListing(id)
     except:
         return abort(404,description = 'Error occured during deletion')
 
 @app.route('/createlisting/<int:id>')
 def createlisting(id):
-    return testlogic.createListing(id)
+    return Repository.createListing(id)
 
 @app.route('/adddescription/<int:id>/<string:text>')
 def adddescription(id,text):
-    return testlogic.addDescription(id,text)
+    return Repository.addDescription(id,text)
 
 @app.route('/deletedescription/<int:id>')
 def deletedescription(id):
-    return testlogic.deleteDescription(id)
+    return Repository.deleteDescription(id)
 
 
 
